239129300938e13be6ba0b47446315b5293a8868
Flavius Turcu <flv.trc@outlook.com>
2022-03-28 16:55:43 +0300

    Initial Commit

---
+++ HIITFitApp/ByeView.swift
@@ -0,0 +1,23 @@
+//
+//  ByeView.swift
+//  HIITFitApp
+//
+//  Created by Flavius on 28/03/2022.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Hello again, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            ByeView()
+            ByeView()
+        }
+    }
+}
---
+++ HIITFitApp/HIITFitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitAppApp.swift
+//  HIITFitApp
+//
+//  Created by Flavius on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
